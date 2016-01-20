e2f2d847e4b65a6dc10171fbb37ad6c7051a2677
Chaz <cmtlum73@gmail.com>
2016-01-20 10:33:36 -1000

    properly setup my table, i can't instructions

--- todo_app.sql
+++ todo_app.sql
@@ -1,6 +1,6 @@
 DROP USER IF EXISTS michael;
 
-CREATE USER micahel WITH ENCRYPTED PASSWORD 'stonebreaker';
+CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 
 DROP DATABASE IF EXISTS todo_app;
 
@@ -8,10 +8,21 @@ CREATE DATABASE todo_app;
 
 \c todo_app;
 
-CREATE TABLE "tasks" ("id" serial NOT NULL, PRIMARY KEY("id"));
+CREATE TABLE "tasks" ("id" serial NOT NULL,
+  PRIMARY KEY("id"),
+  "title" varchar(255) NOT NULL,
+  "description" text NULL,
+  "created_at" timestamp without time zone NOT NULL DEFAULT now(),
+  "updated_at" timestamp without time zone NULL,
+  "completed" boolean NOT NULL DEFAULT false
+  );
 
 ALTER TABLE tasks DROP COLUMN completed;
 
 ALTER TABLE tasks ADD COLUMN "completed_at:" timestamp DEFAULT NULL;
+
+\dt
+
+\d tasks;
 
 SELECT * FROM tasks;
