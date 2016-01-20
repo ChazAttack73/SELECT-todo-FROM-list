b64ce7ce62bad83d5c0811af2ca0bb1e7fd8bc95
Chaz <cmtlum73@gmail.com>
2016-01-19 20:21:45 -1000

    drops a database todo_app if it exists

--- todo_app.sql
+++ todo_app.sql
@@ -2,7 +2,10 @@ DROP USER IF EXISTS michael;
 
 CREATE USER micahel WITH ENCRYPTED PASSWORD 'stonebreaker';
 
+DROP DATABASE IF EXISTS todo_app;
+
 CREATE DATABASE todo_app;
+
 \c todo_app;
 
 CREATE TABLE "users"
